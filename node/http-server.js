64f94c0c7b64f57a15f21113b550d701bd97276e
Gianmarco G <giangarof98@gmail.com>
2022-03-23 10:20:51 -0400

    node stuff

--- node/http-server.js
+++ node/http-server.js
@@ -1,14 +1,35 @@
 const http = require('http');
 const PORT = 3000;
+const texts = [
+    {
+        id:0,
+        text:'This is id 0'
+    },
+    {
+        id:1,
+        text:'this is id 1'
+    }
+]
 const server = http.createServer((req,res) => {
-    if(req.url === '/index'){
+    const text = req.url.split('/');
+    if(req.method === 'POST' && text[1] === 'index'){
+        req.on('data', (data) => {
+            const addText = data.toString()
+            console.log('Request: ', addText)
+            texts.push(JSON.parse(addText))
+        })
+        req.pipe(res)
+    } else if(req.method === 'GET' && text[1] === 'index'){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json')
-        res.end(JSON.stringify({
-            id:1,
-            text:'Hello from the server'
-        }));
-    } else if(req.url === '/msg'){
+        // res.end(JSON.stringify());
+        if(text.length === 3){
+            const textIndex = Number(text[2]);
+            res.end(JSON.stringify(texts[textIndex]));
+        } else{
+            res.end(JSON.stringify(texts));
+        }
+    } else if(req.method === 'GET' && text[1] === 'msg'){
         res.setHeader('Content-Type', 'text/html')
         res.write('<h1>Hello</h1>')
 
