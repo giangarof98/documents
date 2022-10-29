f70bc9950064a98ff681ea4e4702a7afff8fb055
Gianmarco G <giangarof98@gmail.com>
2022-10-28 21:39:12 -0400

    tsc examples

--- demo_app/features/types.ts
+++ demo_app/features/types.ts
@@ -57,3 +57,78 @@ let zero: boolean | number = false;
 for (let i = 0; i < number.length; i++){
     number[i] ? 0 : zero = number[i]
 }
+
+//assign the type to the function
+const add = (a:number, b:number): number => {
+    return a + b;
+}
+
+//Define the type in different functions
+
+function subs(a:number, b:number): number{
+    return a - b
+}
+
+const divide = function(a:number, b:number): number{
+    return a / b;
+}
+
+//Void
+const v = function(a:number, b:number): void{
+    return undefined;
+}
+
+//Destructuring functions
+const forecast = {
+    date: new Date(),
+    day: 'today'
+}
+//first way
+const forecastWeather1 = (forecast: {date: Date, day: string}):void => {
+    console.log(forecast.date, forecast.day)
+}
+//second way
+const forecastWeather2 = ({date, day}: {date: Date, day: string}):void => {
+    console.log(date, day)
+}
+
+
+
+//objects
+
+const person = {
+    personName: 'jhon',
+    age:20,
+    location: {
+        lat: 20,
+        lon: 0
+    },
+    setAge(age:number): void{
+        this.age = age;
+    },
+    setName(name:string): void{
+        this.name = name;
+    }
+}
+
+const {age, personName}: {age:number; personName:string} = person;
+const {location: {lat, lon}}: {location: {lat:number, lon:number}} = person;
+
+
+//Arrays
+
+const cars: string[] = ['toyota','subaru','corolla','camry']
+
+const carsArray: string[][] = [
+    ['toyota'],
+    ['hyundai'],
+    ['camaro']
+]
+
+cars.map((car:string): string => {
+    return car
+})
+
+//Multiple arrays types
+
+const carMakeByYear: (number | string)[] = [2020, 'toyota']
